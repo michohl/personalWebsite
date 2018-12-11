257881a5663fdae90fa60ce2bd1ca9177f8bdf0a
Michael Riesberg-Timmer <Michael82097@gmail.com>
2018-12-11 16:40:53 -0600

    Change index from html to php

--- index.php
+++ index.php
@@ -1,50 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-        <script src="https://use.fontawesome.com/94f29caf94.js"></script>
-
-        
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-        <!-- Google fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Exo:900" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Cinzel:700" rel="stylesheet"> 
-
         <title>MR-T's Portfolio</title>
-        <meta charset="UTF-8">
     </head>
 
-    <style>
-        i{
-            margin-right: 2%;
-        }
-
-        a {
-            color: black;
-        }
-    </style>
-
     <body>
-        <div class="row">
-            <div class="col s12">
-                <h4 style="font-family: 'Cinzel', serif" class="center">
-                    <a href="/">Michael Riesberg-Timmer</a>
-                </h4>
-                <p style="font-family: 'Cinzel', serif" class="center">Computer Science &#8231; IT &#8231; Programmer</p>
-            </div>    
-        </div>
-        <div class="container" style="padding-bottom: 30px">
-            <div class="divider"></div>
-        </div>
+        <?php require_once('Functions/navbar.php'); ?>
 
         <section>
             <h2 style="font-family: 'Cinzel', serif" class="center">Where to Find Me?</h2>
