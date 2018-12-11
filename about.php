b04ae336061e5bef202cc3b312a0e4cf1fd2947f
Michael Riesberg-Timmer <Michael82097@gmail.com>
2018-12-11 15:23:59 -0600

    use new header and materialize

--- about.php
+++ about.php
@@ -1,31 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Michael Riesberg-Timmer's Portfolio</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/circles.css">
-  <link rel="stylesheet" href="/css/navbar.css">
+  <title>MR-T's Portfolio</title>
+</head>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<body>
+  <?php require_once('Functions/navbar.php');?>
 
-  </head>
-
-  <body>
-    <?php require_once('Functions/navbar.php');?>
-
-    <!-- Page Header -->
-    <div class="jumbotron text-center">
-        <h1>About This Site</h1>
-
-        <p>This website was authored by Michael Riesberg-Timmer</p>
-        <p>Launch date: July 21st, 2016</p>
-        <p>Last updated: June 11th, 2018</p>
-        <p>My contact email is: <a href="mailto:Me@MichaelTimmer.me" style="color: blue">Me@MichaelTimmer.me</a></p>
-        <p>The github repo for this website can be found <a href="https://github.com/michohl/personalWebsite" style="color: blue">HERE</a></p>
+    <div class='row'>
+      <div class='col s4 offset-s4'>
+        <div class='card'>
+          <div class='card-content'>
+            <center>
+              <h1>About This Site</h1>
+              <p>This website was authored by Michael Riesberg-Timmer</p>
+              <p>Launch date: July 21st, 2016</p>
+              <p>Last updated: December 11th, 2018</p>
+              <p>My contact email is: <a href="mailto:Me@MichaelTimmer.me" style="color: blue">Me@MichaelTimmer.me</a></p>
+              <p>The github repo for this website can be found <a href="https://github.com/michohl/personalWebsite" style="color: blue">HERE</a></p>
+            </center>
+        </div>
+      </div>
     </div>
-  </body>
+  </div>
+</body>
   
 </html>
